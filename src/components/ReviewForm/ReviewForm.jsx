109d84d70fb9e0a54bea224bd57efc1927355e00
proabdallami <proabdallami@gmail.com>
2025-09-21 15:07:57 +0000

    review

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import "./ReviewForm.css";
+import Review from "./Review";
+
 const ReviewForm = () => {
-    return ( <div>Review Form Component</div> );
-}
+	const [showForm, setShowForm] = useState(false);
+
+	return (
+		<>
+			<div className="review">
+				<h3>Review</h3>
+				<table>
+					<tr>
+						<th>Serial number</th>
+						<th>Doctor Name</th>
+						<th>Doctor Specialty</th>
+						<th>Proivde feedback</th>
+						<th>Review given</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Dr. Smith</td>
+						<td>Cardiology</td>
+						<td>
+							<button onClick={() => setShowForm(true)}>Provide feedback</button>
+						</td>
+						<td>No review given</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Dr. Johnson</td>
+						<td>Neurology</td>
+						<td>
+							<button onClick={() => setShowForm(true)}>Provide feedback</button>
+						</td>
+						<td>No review given</td>
+					</tr>
+				</table>
+			</div>
+			{showForm && <Review />}
+		</>
+	);
+};
 
 export default ReviewForm;
+
